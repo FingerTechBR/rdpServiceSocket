f48beb95ac20f6289f542843ab04b87cdd0c5080
LincolnBerlick <lincoln.berlick@gmail.com>
2019-11-05 17:20:57 -0300

    up files

---
+++ utilsNitgen.cs
@@ -0,0 +1,73 @@
+﻿using NITGEN.SDK.NBioBSP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rdpServiceSocket
+{
+    class utilsNitgen
+    {
+
+        public string Capturar()
+        {
+            NBioAPI m_NBioAPI = new NBioAPI();
+
+            NBioAPI.Type.HFIR hCapturedFIR = new NBioAPI.Type.HFIR();
+            NBioAPI.Type.FIR_TEXTENCODE texto = new NBioAPI.Type.FIR_TEXTENCODE();
+            // Get FIR data
+
+            m_NBioAPI.OpenDevice(255);
+
+            m_NBioAPI.Capture(out hCapturedFIR);
+            m_NBioAPI.GetTextFIRFromHandle(hCapturedFIR, out texto, true);
+
+
+            return texto.TextFIR;
+
+
+
+
+        }
+
+        public string Enroll()
+        {
+            NBioAPI m_NBioAPI = new NBioAPI();
+            NBioAPI.Type.FIR_TEXTENCODE m_textFIR;
+            NBioAPI.Type.HFIR NewFIR;
+            NBioAPI.IndexSearch m_IndexSearch = new NBioAPI.IndexSearch(m_NBioAPI);
+
+
+            NBioAPI.Type.WINDOW_OPTION m_WinOption = new NBioAPI.Type.WINDOW_OPTION();
+            m_WinOption.WindowStyle = (uint)NBioAPI.Type.WINDOW_STYLE.NO_WELCOME;
+
+            string Retorno = "";
+
+            m_NBioAPI.OpenDevice(NBioAPI.Type.DEVICE_ID.AUTO);
+            uint ret = m_NBioAPI.Enroll(out NewFIR, null);
+
+            //uint ret = m_NBioAPI.Enroll(null, out NewFIR, null, NBioAPI.Type.TIMEOUT.DEFAULT, null, m_WinOption);
+
+
+            if (ret != NBioAPI.Error.NONE)
+            {
+                m_NBioAPI.CloseDevice(NBioAPI.Type.DEVICE_ID.AUTO);
+            }
+
+            if (NewFIR != null)
+            {
+                m_NBioAPI.GetTextFIRFromHandle(NewFIR, out m_textFIR, true);
+
+
+                if (m_textFIR.TextFIR != null)
+                {
+                    m_NBioAPI.CloseDevice(NBioAPI.Type.DEVICE_ID.AUTO);
+                    Retorno = m_textFIR.TextFIR.ToString();
+                }
+            }
+            return Retorno;
+        }
+    }
+}
+
