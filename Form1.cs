3798cd2f8cc9690eb19ed14f2265dd5d7df67fff
LincolnBerlick <lincoln.berlick@gmail.com>
2019-11-06 12:59:36 -0300

    alteração estrutura

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,13 +18,15 @@ namespace rdpServiceSocket
     {
 
         TcpListener server;
-
         utilsNitgen utils = new utilsNitgen();
+        Thread thread;
 
         public Form1()
         {
+
             InitializeComponent();
-            Server();
+            thread =new Thread(Server);
+            thread.Start();
         }
 
         private void Server()
@@ -35,13 +38,9 @@ namespace rdpServiceSocket
             {
 
                 Int32 port = 13000;
-
-
                 IPAddress ip = IPAddress.Parse(File.ReadAllText(@"C:\\Windows\\fingertechts.ini"));
                 server = new TcpListener(ip, port);
-
                 server.Start();
-
                 Byte[] bytes = new Byte[15000];
                 String data = null;
 
@@ -51,56 +50,74 @@ namespace rdpServiceSocket
 
                     String digital = null;
                     TcpClient client = server.AcceptTcpClient();
-                    Console.WriteLine("Connected!");
+                    //Console.WriteLine("Connected!");
 
                     data = null;
                     NetworkStream stream = client.GetStream();
                     int i = stream.Read(bytes, 0, bytes.Length);
                     data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
 
-
-                    switch (data)
-                    {
-                        case "0":
-                          
-
-                            digital = utils.Enroll();
-                          
-                            break;
-                        case "1":
-                            try
-                            {
-                                
+                  
+                        switch (data)
+                        {
+                            case "0":
+                                digital = utils.Enroll();
+                                break;
+                            case "1":
                                 digital = utils.Capturar();
                                 break;
-                            }
-                            catch (Exception e)
-                            {
-
-                                break;
-                            }
-
-
-
-
-
+                        }
+                    if (digital == null)
+                    {
+                        lb_status.BeginInvoke(new Action(() =>
+                        {
+                            lb_status.Text = "Não foi possível Capturar digital, verifique conexão";
+                        }));
+                        digital = "";
                     }
-                    //Converter para array de byte
 
+
+
+                    
+                    //Converter para array de byte
                     byte[] msg = System.Text.Encoding.ASCII.GetBytes(digital);
                     //envia resposta de volta
                     stream.Write(msg, 0, msg.Length);
                     client.Close();
+
+
                    
                 }
             }
             catch (SocketException e)
             {
+                lb_status.Text = "Não foi possível Capturar digital, verifique conexão";
+
+
                
-            }
+            }           
+
+
+
+        }
+
+
+
+         private  void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+            server.Stop();
+            thread.Abort();
+
             
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
 
+        }
 
+        private void Label2_Click(object sender, EventArgs e)
+        {
 
         }
     }
--- utilsNitgen.cs
+++ utilsNitgen.cs
@@ -10,55 +10,55 @@ namespace rdpServiceSocket
     class utilsNitgen
     {
 
+        NBioAPI m_NBioAPI = new NBioAPI();
+
+       
+
         public string Capturar()
         {
-            NBioAPI m_NBioAPI = new NBioAPI();
+            
 
             NBioAPI.Type.HFIR hCapturedFIR = new NBioAPI.Type.HFIR();
             NBioAPI.Type.FIR_TEXTENCODE texto = new NBioAPI.Type.FIR_TEXTENCODE();
-            // Get FIR data
-
-            m_NBioAPI.OpenDevice(255);
-
+            //Get FIR data
+            uint device = m_NBioAPI.OpenDevice(255);
+            if (device != NBioAPI.Error.NONE)
+            {
+                return null;
+                
+            }
             m_NBioAPI.Capture(out hCapturedFIR);
             m_NBioAPI.GetTextFIRFromHandle(hCapturedFIR, out texto, true);
-
-
+            m_NBioAPI.CloseDevice(255);
             return texto.TextFIR;
-
-
-
 
         }
 
         public string Enroll()
         {
-            NBioAPI m_NBioAPI = new NBioAPI();
+            
             NBioAPI.Type.FIR_TEXTENCODE m_textFIR;
             NBioAPI.Type.HFIR NewFIR;
             NBioAPI.IndexSearch m_IndexSearch = new NBioAPI.IndexSearch(m_NBioAPI);
 
-
             NBioAPI.Type.WINDOW_OPTION m_WinOption = new NBioAPI.Type.WINDOW_OPTION();
             m_WinOption.WindowStyle = (uint)NBioAPI.Type.WINDOW_STYLE.NO_WELCOME;
-
             string Retorno = "";
-
-            m_NBioAPI.OpenDevice(NBioAPI.Type.DEVICE_ID.AUTO);
-            uint ret = m_NBioAPI.Enroll(out NewFIR, null);
-
-            //uint ret = m_NBioAPI.Enroll(null, out NewFIR, null, NBioAPI.Type.TIMEOUT.DEFAULT, null, m_WinOption);
-
-
-            if (ret != NBioAPI.Error.NONE)
+             m_NBioAPI.OpenDevice(255);
+            uint device = m_NBioAPI.OpenDevice(255);
+            if (device != NBioAPI.Error.NONE)
             {
-                m_NBioAPI.CloseDevice(NBioAPI.Type.DEVICE_ID.AUTO);
+                return null;
+
             }
 
+
+            uint ret = m_NBioAPI.Enroll(out NewFIR, null);
+            //uint ret = m_NBioAPI.Enroll(null, out NewFIR, null, NBioAPI.Type.TIMEOUT.DEFAULT, null, m_WinOption);
+           
             if (NewFIR != null)
             {
                 m_NBioAPI.GetTextFIRFromHandle(NewFIR, out m_textFIR, true);
-
 
                 if (m_textFIR.TextFIR != null)
                 {
@@ -66,6 +66,7 @@ namespace rdpServiceSocket
                     Retorno = m_textFIR.TextFIR.ToString();
                 }
             }
+            m_NBioAPI.CloseDevice(255);
             return Retorno;
         }
     }
