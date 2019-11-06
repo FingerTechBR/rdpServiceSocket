5ee6fcae766b76a7c5004838e53e34eac226261e
LincolnBerlick <lincoln.berlick@gmail.com>
2019-11-06 16:12:09 -0300

    v1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,12 +31,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.lb_status = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lb_ip = new System.Windows.Forms.Label();
+            this.lb_ip_req = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1, 174);
+            this.label1.Location = new System.Drawing.Point(2, 80);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(40, 13);
             this.label1.TabIndex = 0;
@@ -45,7 +49,7 @@
             // label2
             // 
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label2.Location = new System.Drawing.Point(-6, 172);
+            this.label2.Location = new System.Drawing.Point(1, 70);
             this.label2.Margin = new System.Windows.Forms.Padding(0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(304, 2);
@@ -56,22 +60,67 @@
             // lb_status
             // 
             this.lb_status.AutoSize = true;
-            this.lb_status.Location = new System.Drawing.Point(48, 174);
+            this.lb_status.Location = new System.Drawing.Point(48, 80);
             this.lb_status.Name = "lb_status";
             this.lb_status.Size = new System.Drawing.Size(0, 13);
             this.lb_status.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(2, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ip da ultima requisição :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(2, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ip Local :";
+            // 
+            // lb_ip
+            // 
+            this.lb_ip.AutoSize = true;
+            this.lb_ip.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_ip.Location = new System.Drawing.Point(67, 11);
+            this.lb_ip.Name = "lb_ip";
+            this.lb_ip.Size = new System.Drawing.Size(0, 17);
+            this.lb_ip.TabIndex = 5;
+            // 
+            // lb_ip_req
+            // 
+            this.lb_ip_req.AutoSize = true;
+            this.lb_ip_req.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_ip_req.Location = new System.Drawing.Point(156, 39);
+            this.lb_ip_req.Name = "lb_ip_req";
+            this.lb_ip_req.Size = new System.Drawing.Size(0, 17);
+            this.lb_ip_req.TabIndex = 6;
+            this.lb_ip_req.Click += new System.EventHandler(this.Lb_ip_req_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(297, 196);
+            this.ClientSize = new System.Drawing.Size(297, 102);
+            this.Controls.Add(this.lb_ip_req);
+            this.Controls.Add(this.lb_ip);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.lb_status);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,6 +131,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lb_status;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb_ip;
+        private System.Windows.Forms.Label lb_ip_req;
     }
 }
 
